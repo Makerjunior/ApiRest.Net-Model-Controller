ccd74beaa932052e72af4fd1a1246152264b6dcd
Junior Maker <92755114+Makerjunior@users.noreply.github.com>
2023-03-11 02:06:50 -0300

    Campos Adicionados

--- ApiTexte/Models/TodoItem.cs
+++ ApiTexte/Models/TodoItem.cs
@@ -4,5 +4,8 @@ public class TodoItem
 {
     public long Id { get; set; }
     public string? Name { get; set; }
+    public string? Endress { get; set; }
+    public string? Status { get; set; }
+  
     public bool IsComplete { get; set; }
 }
